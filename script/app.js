9e713ea0796d5c079cc67cb3679c471b06f062f2
bozar <5583771+Bozar@users.noreply.github.com>
2021-08-10 13:20:25 +0800

    Validate & print input text.

--- script/app.js
+++ script/app.js
@@ -3,10 +3,33 @@
 const app = {}
 
 app.onClickButton = function () {
-  console.log(`Click: ${this.id}`)
+  const inputSource = document.getElementById('inputSource')
+  const inputMatch = document.getElementById('inputMatch')
+  const inputReplace = document.getElementById('inputReplace')
+  const outputSource = document.getElementById('outputSource')
+
+  if (inputSource.value === '') {
+    window.alert('Source string required.')
+    return
+  } else if (inputMatch.value === '') {
+    window.alert('Match pattern required.')
+    return
+  } else if ((this.id === 'replaceButton') && (inputReplace.value === '')) {
+    window.alert('Replace pattern required.')
+    return
+  }
+
+  outputSource.innerHTML = inputSource.value
+}
+
+app.clearInput = function () {
+  for (const i of document.getElementsByClassName('regExpInput')) {
+    i.value = ''
+  }
 }
 
 window.onload = function () {
+  app.clearInput()
   for (const i of document.getElementsByClassName('regExpButton')) {
     i.addEventListener('click', app.onClickButton)
   }
